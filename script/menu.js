b7930cbbf85f621e5d94dd0e91bedfd935a1d554
guicarbar <guicarbar@gmail.com>
2024-09-26 12:09:30 -0300

    refazendo o header
    estou refazendo cada componete pois anteriormente era apenas um projeto de como seria

--- script/menu.js
+++ script/menu.js
@@ -1,5 +1,5 @@
-async function menu() {
-    let list = document.getElementById("menu_btn")
+function menu() {
+    let list = document.getElementById("header_menu_list")
 
     if (list.style.display == "none") {
         list.style.display = "flex"
