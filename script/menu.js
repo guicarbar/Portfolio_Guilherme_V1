a3b005474f190880fab9f37af5ffb9777b249ae3
guicarbar <guicarbar@gmail.com>
2024-09-25 22:46:20 -0300

    corrigindo menu

--- script/menu.js
+++ script/menu.js
@@ -2,7 +2,7 @@ async function menu() {
     let list = document.getElementById("menu_btn")
 
     if (list.style.display == "none") {
-        list.style.display = "block"
+        list.style.display = "flex"
     } else {
         list.style.display = "none"
     }
